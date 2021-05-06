foxeng/libzseek

commit 9fee09019d27bcfbe5a7f3a9fec35c1ee4a6924a

Prevent concurrent file modifications

src/compress.c
@@ -86,7 +86,7 @@ zseek_writer_t *zseek_writer_open(const char *filename, int nb_workers,
     }
     writer->fl = fl;
 
-    fout = fopen(filename, "wb");
+    fout = fopen(filename, "wbx");
     if (!fout) {
         set_error_with_errno(errbuf, "open file", errno);
         goto fail_w_framelog;

src/decompress.c
@@ -124,7 +124,7 @@ ssize_t zseek_pread(zseek_reader_t *reader, void *buf, size_t count,
 {
     ssize_t frame_idx = 0;
     int pr = 0;
-    void *cbuf = NULL; // Declared here to be in scope at fail_w_cbuf
+    void *cbuf = NULL;
     size_t offset_in_frame = 0;
     size_t to_copy = 0;
 

commit bf760614c1bacd3704d48e81433c8a3e6e6843f8

Fix benchmark: write to regular file
It was broken since 9fee09019d27bcfbe5a7f3a9fec35c1ee4a6924a, which
introduced the restricion for existing output files.

test/benchmark.c
@@ -248,14 +248,20 @@ int main(int argc, char *argv[])
     }
 
     const char *ufilename = argv[1];
-    const char *cfilename = "/dev/null";
+    char *cfilename = malloc(strlen(ufilename) + 5);
+    if (!cfilename) {
+        perror("allocate output filename");
+        goto fail;
+    }
+    strcpy(cfilename, ufilename);
+    strcat(cfilename, ".zst");
 
     int nb_workers = atoi(argv[2]);
     size_t frame_size = atoi(argv[3]) * (1 << 20);
 
     results_t *res = compress(ufilename, cfilename, nb_workers, frame_size);
     if (!res)
-        return 1;
+        goto fail_w_cfilename;
 
     bool terse = false;
     if (argc > 4 && strcmp(argv[4], "-t") == 0) {
@@ -264,4 +270,12 @@ int main(int argc, char *argv[])
     report(res, nb_workers, terse);
 
     results_free(res);
+    free(cfilename);
+
+    return 0;
+
+fail_w_cfilename:
+    free(cfilename);
+fail:
+    return 1;
 }