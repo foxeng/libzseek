8c2beb5892251cca88ddd4ce5abb82faa3e093a7
Fotis Xenakis <foxen@windowslive.com>
2021-09-09 18:01:26 +0300

    Add zstd copyright notice

--- src/seek_table.c
+++ src/seek_table.c
@@ -1,12 +1,3 @@
-/*
- * Copyright (c) 2017-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under both the BSD-style license (found in the
- * LICENSE file in the root directory of this source tree) and the GPLv2 (found
- * in the COPYING file in the root directory of this source tree).
- */
-
 #include <stdint.h>     // uint*_t
 #include <stddef.h>     // size_t
 #include <stdbool.h>    // bool
